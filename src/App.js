2b82644d2b11ff997c3c341904851e1aa96246fa
vsaiprathapreddy <saiprathap3424@gmail.com>
2021-07-21 18:42:15 +0530

    Final changes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Navbar from './Navbar';
 
 import 'bootstrap/dist/css/bootstrap.css';
 import Weather from  './Weather.js'
-import navbar from './Navbar';
 function App() {
   return (
     <div className="App">
-      <Navbar />
+      
     <h1 style={{backgroundColor:'#2E8B57',fontStyle:'YELLOWTAIL'}}>WEATHER</h1>
         <Weather />
     </div>
